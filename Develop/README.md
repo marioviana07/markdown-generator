86634cc1f39d875e0a2c19f24d355d2b9b06fbfd
marioviana07 <mario_viana_2005@yahoo.com>
2020-08-01 22:05:17 -0700

    generate the readme

--- Develop/README.md
+++ Develop/README.md
@@ -1,2 +1,9 @@
-# asd
-
+Please email me if you have questions at the above email address#[Markdown Generator](#Markdown Generator) &middot; undefined
+ ## Table of Contents
+*[Installation](#Installation)
+*[Usage](#Usage)
+*[Contributing](#Contribution)
+*[Test](#Test)
+*[Questions](#Questions)
+# Installation
+`node install`
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -10,8 +10,35 @@ const mapOfKeyToMarkdDown = new Map([
     ['email', 'User E-mail:'],
 ]);
 
+const mapOfLicenseToBadge = new Map([
+    ['Apache License 2.0', '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)']
+]);
 
+function generateMarkdown(data) {
 
+    let results = '';
+    const keys = Object.keys(data)
+    console.log("KEYS =>", keys);
+    keys.forEach(key => {
+        if (key == 'title') {
+            const license = data.license;
+            const title = data[key];
+            results += `#[${title}](#${title}) &middot; ${mapOfLicenseToBadge.get(license)}`;
+        } else if (key == 'description') {
+            results += `\n ## Table of Contents`;
+            results += `\n*[Installation](#Installation)`;
+            results += `\n*[Usage](#Usage)`;
+            results += `\n*[Contributing](#Contribution)`;
+            results += `\n*[Test](#Test)`;
+            results += `\n*[Questions](#Questions)`;
+        } else if (key == 'email') {
+            results += 'Please email me'
+        } else if (key == 'installation') {
+            results += `\n# Installation\n\`${data[key]}\`\n`;
+        }
+    });
+    return results;
+}
 
 
 
