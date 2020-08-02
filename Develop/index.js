e31bf758b5edf16ccdb12b9fd86076626e308c62
marioviana07 <mario_viana_2005@yahoo.com>
2020-08-01 19:27:02 -0700

    First commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,16 @@
+const inquirer = inquirer('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
 // array of questions for user
 const questions = [
 
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
